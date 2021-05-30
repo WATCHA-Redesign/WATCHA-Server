43fb6fc771ac7caa8135601be07fa97eb3035ab8
SumiKang <65805437+SumiKang@users.noreply.github.com>
2021-05-30 17:04:43 +0900

    Update SearchHigh.ts

--- WATCHA-Server/src/models/SearchHigh.ts
+++ WATCHA-Server/src/models/SearchHigh.ts
@@ -3,7 +3,8 @@ import { IHigh } from "../interfaces/IHigh";
 
 const SearchHighSchema = new mongoose.Schema({
     image:{type:String},
-    name:{type:String}
+    title:{type:String},
+    subtitle:{type:String}
 })
 
 export default mongoose.model<IHigh & mongoose.Document>("SearchHigh",SearchHighSchema)
